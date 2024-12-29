3117c87f6c65081cd0fda53b95f0d282c94d463e
Jacky Jiang <jiang12j@gmail.com>
2024-12-28 23:06:18 -0500

    Created component headbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
-
+import Headbar from "../components/Headbar";
 
 export default function Home() {
     return (
-        <div className="flex justify-center m-[2%] h-[10vh]">
-            <div style={{ backgroundColor:'beige'}} className="flex justify-between border-solid border-2 border-black w-full h-full p-[1%] rounded-md shadow-lg">
-                <h1 className="float-left p-[1%]">New Jangout</h1>
-                <button className="float-right pr-[1%] border-black border-2 flex justify-center items-center">Login</button>
-            </div>
+        <div>
+            <Headbar/>
         </div>
     );
 }
